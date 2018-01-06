17da508e4dd065cc0bb1a8d5466d4c558555432b
POLSKA\x <jakub.dardzinski@lingaro.com>
2018-01-06 21:18:12 +0100

    Add BFS

--- generated_graph.py
+++ generated_graph.py
@@ -13,16 +13,19 @@ class Node(object):
         self.number = number
         self.neighbors = []
         self.visited = None
+        
+    def __repr__(self):
+        return "%s, %s, nr = %s" % (self.x, self.y, self.number)
 
 class GeneratedGraph(object):
     __metaclass__ = ABCMeta
     f = plt.figure(figsize=(7, 7), dpi=100)
     sp = f.add_subplot(111)
-    no_of_nodes = 100
+    no_of_nodes = 50
     
     def __init__(self):
         self.generate_nodes()
-        checked_nodes = [0]
+        checked_nodes = []
         for node1 in self.nodes:
             checked_nodes.append(node1)
             for node2 in self.nodes:
@@ -30,6 +33,16 @@ class GeneratedGraph(object):
                     continue
                 if self.check_connection(node1, node2):
                     self.connect_nodes(node1, node2)
+                    
+        self.find_largest_connected_graph()
+        checked_nodes = []
+        for node1 in self.nodes:
+            checked_nodes.append(node1)
+            for node2 in self.nodes:
+                if node2 in checked_nodes:
+                    continue
+                if node2 in node1.neighbors:
+#                     self.connect_nodes(node1, node2)
                     self.draw_connected_nodes(node1, node2)
     
     def generate_nodes(self):
@@ -53,6 +66,30 @@ class GeneratedGraph(object):
     def draw_connected_nodes(self, node1, node2, color = "C0"):
         self.sp.plot([node1.x, node2.x], [node1.y, node2.y], color+"-", lw=0.2)
     
+    def find_highest_degree(self):
+        return max(self.nodes, key=lambda x: len(x.neighbors))
+    
+    def bfs(self):
+        q = [self.find_highest_degree()]
+        q[0].visited = True
+        nodes = set(q)
+        while len(q) > 0:
+            v = q.pop()
+            for node in v.neighbors:
+                if not node.visited:
+                    q.append(node)
+                    node.visited = True
+                    nodes.add(node)
+#         print nodes
+        return nodes
+    
+    def find_largest_connected_graph(self):
+        to_throw = set(self.nodes) - self.bfs()
+#         print set(self.nodes) - self.bfs(), len(set(self.nodes) - self.bfs())
+        self.nodes = [node for node in self.nodes if not node in to_throw]
+        print len(self.nodes)
+
+
 #     def remove_node(self, node, from_whom):
 #         for neighbor in node.neighbors:
 #             if neighbor is not from_whom:
@@ -68,7 +105,7 @@ class GeneratedGraph(object):
         
         
 class BernoulliGraph(GeneratedGraph):
-    r = 0.1
+    r = 0.15
     
     def calc_dist(self, node1, node2):
         return math.sqrt(math.pow(node1.x-node2.x, 2)+math.pow(node1.y-node2.y, 2))
